fad54464c956ce2e3a21a365946f47dff9dac90b
lzx <3263760852@qq.com>
2024-12-05 06:09:22 +0000

    test docker

--- docker/build_and_run.sh
+++ docker/build_and_run.sh
@@ -1,11 +1,12 @@
 sudo docker build -t cuda11.8-opengl-ubuntu22.04 --network host .
 sudo xhost +
 sudo docker run -it --name=gs_semantic_slam_cudagl_11.8  --network host \
-	--gpus all -v $HOME/docker_temp/gs_slam_temp_data:/home/data \
+	--gpus '"device=0,1"' -v $HOME/docker_temp/gs_slam_temp_data:/home/data \
 	-v /tmp/.X11-unix:/tmp/.X11-unix --device=/dev/dri --group-add video --env=DISPLAY=$DISPLAY --env=QT_X11_NO_MITSHM=1 \
 	--device=/dev/nvidia-uvm \
-    --device=/dev/nvidia-uvm-tools \
-    --device=/dev/nvidia-modeset \
-    --device=/dev/nvidiactl \
-    --device=/dev/nvidia0 \
-  	cuda11.8-opengl-ubuntu22.04:latest /bin/bash
+  --device=/dev/nvidia-uvm-tools \
+  --device=/dev/nvidia-modeset \
+  --device=/dev/nvidiactl \
+  --device=/dev/nvidia0 \
+  --device=/dev/nvidia1 \
+  cuda11.8-opengl-ubuntu22.04:latest /bin/bash
