5edbda9976cf5864663fd3fd5c0414bc2457a0eb
lzx <3263760852@qq.com>
2024-12-20 20:21:46 +0800

    update docker file

--- docker/build_and_run.sh
+++ docker/build_and_run.sh
@@ -1,12 +1,13 @@
 sudo docker build -t cuda11.8-opengl-ubuntu22.04 --network host .
-sudo xhost +
-sudo docker run -it --name=gs_semantic_slam_cudagl_11.8  --network host \
-	--gpus '"device=0,1"' -v $HOME/docker_temp/gs_slam_temp_data:/home/data \
-	-v /tmp/.X11-unix:/tmp/.X11-unix --device=/dev/dri --group-add video --env=DISPLAY=$DISPLAY --env=QT_X11_NO_MITSHM=1 \
-	--device=/dev/nvidia-uvm \
-  --device=/dev/nvidia-uvm-tools \
-  --device=/dev/nvidia-modeset \
-  --device=/dev/nvidiactl \
-  --device=/dev/nvidia0 \
-  --device=/dev/nvidia1 \
-  cuda11.8-opengl-ubuntu22.04:latest /bin/bash
+
+sudo xhost + 
+export DISPLAY=:1
+sudo docker run -it --name=gsdff_slam_cudagl_11.8  --network host \
+	--gpus '"device=2,3"' -v $DATA_DISK:/home/data \
+	-v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri:/dev/dri \
+    --device=/dev/dri --group-add video \
+    --env=DISPLAY=$DISPLAY --env=QT_X11_NO_MITSHM=1 \
+    --env=__NV_PRIME_RENDER_OFFLOAD=1 \
+    --env=__GLX_VENDOR_LIBRARY_NAME=nvidia \
+    --env=__NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0 \
+    cuda11.8-opengl-ubuntu22.04:latest /bin/bash
